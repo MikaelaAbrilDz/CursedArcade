ba6d5d8e78bc5a3df534ee3c0f33f50a80ab1d0b
MikaelaAbrilDz <151180915+MikaelaAbrilDz@users.noreply.github.com>
2025-10-12 11:21:17 +0200

    Sistema básico de turnos y personaje moviéndose mediante ellos

--- Assets/CursedArcade/Scripts/PlayerController.cs
+++ Assets/CursedArcade/Scripts/PlayerController.cs
@@ -1,6 +1,46 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerController : BasicEntity
 {
+    public void OnMovementUp(InputAction.CallbackContext context)
+    {
+        TurnManager turnManager = FindAnyObjectByType<TurnManager>();
 
+        if (turnManager.turnList[0] == GetComponent<BasicEntity>() && context.performed)
+        {
+            transform.position = CurrentChecker().sideCheckers[0].transform.position;
+            turnManager.PassTurn(GetComponent<BasicEntity>());
+        }
+    }
+    public void OnMovementRight(InputAction.CallbackContext context)
+    {
+        TurnManager turnManager = FindAnyObjectByType<TurnManager>();
+
+        if (turnManager.turnList[0] == GetComponent<BasicEntity>() && context.performed)
+        {
+            transform.position = CurrentChecker().sideCheckers[1].transform.position;
+            turnManager.PassTurn(GetComponent<BasicEntity>());
+        }
+    }
+    public void OnMovementDown(InputAction.CallbackContext context)
+    {
+        TurnManager turnManager = FindAnyObjectByType<TurnManager>();
+
+        if (turnManager.turnList[0] == GetComponent<BasicEntity>() && context.performed)
+        {
+            transform.position = CurrentChecker().sideCheckers[2].transform.position;
+            turnManager.PassTurn(GetComponent<BasicEntity>());
+        }
+    }
+    public void OnMovementLeft(InputAction.CallbackContext context)
+    {
+        TurnManager turnManager = FindAnyObjectByType<TurnManager>();
+
+        if (turnManager.turnList[0] == GetComponent<BasicEntity>() && context.performed)
+        {
+            transform.position = CurrentChecker().sideCheckers[3].transform.position;
+            turnManager.PassTurn(GetComponent<BasicEntity>());
+        }
+    }
 }
