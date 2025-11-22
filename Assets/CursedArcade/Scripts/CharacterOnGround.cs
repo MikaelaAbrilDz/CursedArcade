b045ce73107c1c793bb39339ff759c990b140144
arnamo230 <arnamo@alumnatflorida.es>
2025-11-23 00:54:50 +0100

    Rotación de los enemigos al correr, implementación de los assets de los enemigos junto con su animación de correr

--- Assets/CursedArcade/Scripts/CharacterOnGround.cs
+++ Assets/CursedArcade/Scripts/CharacterOnGround.cs
@@ -14,6 +14,28 @@ public class CharacterOnGround : BasicEntity
         if (isMoving || CurrentChecker().sideCheckers[directionIndex] == null || CurrentChecker().sideCheckers[directionIndex].positioned != null &&
             CurrentChecker().sideCheckers[directionIndex].positioned.GetType().IsSubclassOf(typeof(CharacterOnGround))) return false;
         isMoving = true;
+
+
+        switch (directionIndex)
+        {
+            case 0:
+                transform.eulerAngles = new Vector3(0f, 0f, 0f);
+                break;
+
+            case 1:
+                transform.eulerAngles = new Vector3(0f, 90f, 0f);
+                break;
+
+            case 2:
+                transform.eulerAngles = new Vector3(0f, 180f, 0f);
+                break;
+
+                case 3:
+                transform.eulerAngles = new Vector3(0f, 270f, 0f);
+                break;
+        }
+
+
         SetPositionedChecker(false);
         LeanTween.move(gameObject, CurrentChecker().sideCheckers[directionIndex].transform.position, 0.2f)
             .setEaseOutQuad().setOnComplete(()=>FinishMovement());
